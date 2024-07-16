4995ff265eb240c46af9bcaafe38391f779a834b
gamoko <103703075+gamoko@users.noreply.github.com>
2024-07-16 23:18:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -51,9 +51,7 @@ def run(args: DictConfig):
     #     Dropout
     # ------------------
     class Dropout(nn.Module):
-    """
-    http://arxiv.org/abs/1207.0580
-    """
+        
     def __init__(self, dropout_ratio=0.5):
         super().__init__()
         self.dropout_ratio = dropout_ratio
